d1b084b7589ba1dc79bdbcb87cdf2849b0cb7171
ab2651 <ab2651@kent.ac.uk>
2024-10-27 16:22:41 +0000

    ListManipulation Code

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,11 @@
 ### Part Three -- your code goes here. 
+list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+list.sort()
+
+n = 2
+newList = list[n:]
+
+extendedList = newList
+extendedList.extend([7, 8])
+
+print(extendedList)
