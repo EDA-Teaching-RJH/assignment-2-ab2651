45c996864cb8683a89884766a66788ee5461120d
ab2651 <ab2651@kent.ac.uk>
2024-10-27 17:10:02 +0000

    AllTogether Code

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,19 @@
 ### Part Four -- your code goes here. 
+import random
+randomList = []
+for i in range(10):
+    n = random.randint(1, 100)
+    randomList.append(n)
+print("The random numbers are:", randomList)
+
+x = 0
+while x < len(randomList):
+    if randomList[x] % 2 == 0:
+        randomList.pop(x)
+    else:
+        x += 1
+print("Odd numbers in the list are:", randomList)
+
+
+
+
